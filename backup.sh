0bb47076d53adf40a82b41d94f254a8485aba870
arjunkundur <arjunkundur@gmail.com>
2017-02-17 14:02:44 +0530

    Backup Of Selected Dir's

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# AUTO BACKUP FOR THE SELECTED DIRECTORIES
+
+HOME_DIR= /home/devops/training/
+
+echo "=========COPYING STARTED=============="
+cp -r $HOME_DIR/ansible $HOME_DIR/DevOps
+cp -r $HOME_DIR/bash $HOME_DIR/DevOps
+cp -r $HOME_DIR/git $HOME_DIR/DevOps
+cp -r $HOME_DIR/jenkins $HOME_DIR/DevOps
+cp -r $HOME_DIR/vagrant $HOME_DIR/DevOps
+echo "=========COPYING COMPLETED============"
+
+git add .
+git commit -m " Backup Of Selected Dir's "
+git push origin master
+
+echo " GIT UPLOAD SUCESSFULL "
+
+
+
---
+++ vagrant/practice/1/first.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+echo " tried for vagrant "
+mkdir /home/devops/33
+touch 1 2 3 
