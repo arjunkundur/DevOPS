48187fcaa9204144315ac0213c2dc6277f595429
arjunkundur <arjunkundur@gmail.com>
2017-02-17 14:43:32 +0530

    Backup Of Selected Dir's

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@
 HOME_DIR= /home/devops/training/
 
 echo "=========COPYING STARTED=============="
-cp -r $HOME_DIR/ansible/ $HOME_DIR/DevOps
-cp -r $HOME_DIR/bash/ $HOME_DIR/DevOps
-cp -r $HOME_DIR/git/ $HOME_DIR/DevOps
-cp -r $HOME_DIR/jenkins/ $HOME_DIR/DevOps
-cp -r $HOME_DIR/vagrant/ $HOME_DIR/DevOps
+cp -r HOME_DIR/ansible/ $HOME_DIR/DevOps
+cp -r HOME_DIR/bash/ $HOME_DIR/DevOps
+cp -r HOME_DIR/git/ $HOME_DIR/DevOps
+cp -r HOME_DIR/jenkins/ $HOME_DIR/DevOps
+cp -r HOME_DIR/vagrant/ $HOME_DIR/DevOps
 echo "=========COPYING COMPLETED============"
 
 git add .
